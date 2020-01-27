23a7a263e0c50deacc9263b3b325732f36270167
Carlos <carlosguillermoru@outlook.com>
2020-01-27 15:40:29 +0100

    Aplica modelo Vista/Controlador. Un paquete para vista dónde guardamos la interfaz y el programa, y un paquete controlador para el resto

--- src/main/java/vista/Interfaz.java
+++ src/main/java/vista/Interfaz.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CafeteraDaw;
+package vista;
 
 /**
  *
  * @author joselu
  */
-public class Administrador {
+public class Interfaz {
     
 }
---
+++ src/main/java/vista/Programa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author joselu
+ */
+public class Programa {
+    //Según el modelo Vista / Controlador, debemos tener algunas clases para la vista, y otras para la lógica del programa. Considerando esto, las clases programa e interfaz deben estar en un paquete vista, mientras que el resto de clases, en un paquete controlador
+}
