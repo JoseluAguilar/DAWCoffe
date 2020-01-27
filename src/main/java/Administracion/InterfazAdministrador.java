614d00a06f10b75bb47f528724143b180957fdcf
Carlos <carlosguillermoru@outlook.com>
2020-01-27 15:43:16 +0100

    Movemos todo lo relacionado con el administrador al paquete administacion

--- src/main/java/Administracion/InterfazAdministrador.java
+++ src/main/java/Administracion/InterfazAdministrador.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controlador;
+package Administracion;
 
 /**
  *
- * @author joselu
+ * @author carlos
  */
-public class Administrador {
+public class InterfazAdministrador {
     
 }
