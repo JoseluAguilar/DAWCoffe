868ec480934763db30d193ffecfac15ea43c05e5
Joselu Aguilar <joselubiker@gmail.com>
2020-01-27 23:05:38 +0100

    Creación de la clase monedero

---
+++ src/main/java/controlador/Monedero.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author josel
+ */
+public class Monedero {
+    //Clase establecida para almacenar el dinero 
+    //Cada vez que la maquina inicie, esta tendra 50€ de cambio
+    //No se permitirá un importe introducido que no podamos devolver cambio
+    //Si se requiriese.
+    private double dinero = 50.00;
+
+    public double getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+    
+    
+    
+}
