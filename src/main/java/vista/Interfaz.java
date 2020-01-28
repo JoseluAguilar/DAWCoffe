6ed6b98a48ba039b6f33a5486337b786ab3e4a2d
Carlos <carlosguillermoru@outlook.com>
2020-01-28 20:43:31 +0100

    Modifica la ejecución del programa

--- src/main/java/vista/Interfaz.java
+++ src/main/java/vista/Interfaz.java
@@ -5,10 +5,64 @@
  */
 package vista;
 
+import controlador.Bebida;
+import controlador.Cafetera;
+import java.util.Scanner;
+import static vista.Programa.valoresBebida;
+
 /**
  *
  * @author joselu
  */
 public class Interfaz {
-    
+    private static Cafetera cafe = new Cafetera();
+    public static void menuPrincipal(){
+        System.out.println("Bienvenido a la cafetera, por Jose Luís y Carlos");
+        int seleccionMenu;
+        cargarBebidas();
+        while (true) {
+            //TODO tenemos que poner aquí los códigos de bebida, así como poner el precio. Podemos hacerlo con un 
+            //toString en la clase Bebida
+            seleccionMenu = menuRaiz(); //Esto lo arreglaremos más adelante, lo dejamos así por ahora
+            cafe.dispensarBebida(seleccionMenu);
+        }
+    }
+    public static int menuRaiz() {
+        //Este método, que tendremos que rellenar más adelante, lo usamos para preguntar la bebida a tomar
+        Scanner lector = new Scanner(System.in);
+        int seleccion;
+        boolean existe = false;
+        do {
+            System.out.println("Introduce la bebida que quieres tomar: ");
+            seleccion = lector.nextInt();
+            //Preguntamos si el código de bebida existe. Si existe, lo devolverá para operar con él
+            //en el método main
+            if(existeEnArray(seleccion)){
+                existe = true;
+            }
+            else{
+                System.out.println("El número introducido no pertenece a ninguna bebida, por favor, introduce otro");
+            }
+        } while (!(existe));
+        return seleccion;
+
+    }
+
+    public static void cargarBebidas() {
+        //Creamos este método para cargar los códigos de bebidas en un array
+        int codBebida = 1000;
+        for (int i = 0; i < valoresBebida.length; i++) {
+            codBebida++;
+            valoresBebida[i] = codBebida;
+        }
+    }
+    public static boolean existeEnArray(int num){
+        //Este método busca y pregunta si un valor existe o no en un array
+        for (int i = 0; i < valoresBebida.length; i++) {
+            if(num == valoresBebida[i]){
+                return true;
+            }
+        }
+        return false;
+    }
 }
