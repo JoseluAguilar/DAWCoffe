addac7ce6a67aa2ec7d8ce9a583792cdd66aab55
Carlos <carlosguillermoru@outlook.com>
2020-01-28 10:10:26 +0100

    Crea un array y métodos para: inicializar array con el número de las bebidas, Comprobar si las bebidas existen (Las mete en un array)

--- src/main/java/vista/Programa.java
+++ src/main/java/vista/Programa.java
@@ -6,6 +6,8 @@
 package vista;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import controlador.*;
 
 /**
  *
@@ -17,16 +19,50 @@ public class Programa {
 
     //Considerando esto, las clases programa e interfaz deben estar en un paquete vista, 
     //mientras que el resto de clases, en un paquete controlador
+    private static int[] valoresBebida = new int[11];
+
     public static void main(String[] args) {
         System.out.println("Bienvenido a la cafetera, por Jose Luís y Carlos");
-        while(true){
-            menuRaiz();
+        int seleccionMenu;
+        cargarBebidas();
+        while (true) {
+            seleccionMenu = menuRaiz();
+
         }
-        
+
     }
-    public static int menuRaiz(){
+
+    public static int menuRaiz() {
         Scanner lector = new Scanner(System.in);
-        System.out.println("Introduce la bebida que quieres tomar: ");
-        return lector.nextInt();
+        int seleccion;
+        boolean existe = false;
+        do {
+            System.out.println("Introduce la bebida que quieres tomar: ");
+            seleccion = lector.nextInt();
+            if(existeEnArray(seleccion)){
+                existe = true;
+            }
+            else{
+                System.out.println("El número introducido no pertenece a ninguna bebida, por favor, introduce otro");
+            }
+        } while (!(existe));
+        return seleccion;
+
+    }
+
+    public static void cargarBebidas() {
+        int codBebida = 1000;
+        for (int i = 0; i < valoresBebida.length; i++) {
+            codBebida++;
+            valoresBebida[i] = codBebida;
+        }
+    }
+    public static boolean existeEnArray(int num){
+        for (int i = 0; i < valoresBebida.length; i++) {
+            if(num == valoresBebida[i]){
+                return true;
+            }
+        }
+        return false;
     }
 }
