87be0d0f83079c154a77c75b871a3905d9e37383
Joselu Aguilar <joselubiker@gmail.com>
2020-01-28 20:24:35 +0100

    Añadir funcionalidad en Administrador

--- src/main/java/Administracion/InterfazAdministrador.java
+++ src/main/java/Administracion/InterfazAdministrador.java
@@ -9,6 +9,16 @@ package Administracion;
  *
  * @author carlos
  */
+import controlador.Deposito;
 public class InterfazAdministrador {
     
+    //Aquí vamos a hacer unos cuanto métodos que necesitamos de Administrador
+    Deposito auxDeposito = new Deposito();
+    public String contadores(){
+        return auxDeposito.toString();
+    }
+    
+    public void Rellenar(){
+        auxDeposito.rellenarCafetera();
+    }
 }
